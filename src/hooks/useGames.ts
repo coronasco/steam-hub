1cd8dbfb5ff8d95b64ae3b1fb832d8f8c4ea0538
Daniel Zaharia <daniel.zaharia.corona@gmail.com>
2023-04-08 21:27:50 +0200

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenre";
 
 export interface Platform {
     id: number;
@@ -23,7 +22,8 @@ const useGames = (gameQuery: GameQuery) => (
             params: { 
                 genres: gameQuery.genre?.id, 
                 platforms: gameQuery.platform?.id,
-                ordering: gameQuery.sortOrder
+                ordering: gameQuery.sortOrder,
+                search: gameQuery.searchText
             }
         }, 
         [gameQuery])
